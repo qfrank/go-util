09abb54722f62762711c3fc78806424b087af562
Frank Tang <lovefree103@gmail.com>
2018-11-05 14:55:54 +0800

    连接mongo-增加无密码连接判断

--- mongo.go
+++ mongo.go
@@ -1,11 +1,19 @@
 package goutil
 
-import "github.com/globalsign/mgo"
+import (
+	"github.com/globalsign/mgo"
+	"strings"
+)
 
 func DialMongoDB(mongodbUrl string) *mgo.Database {
-	di, err := mgo.ParseURL(mongodbUrl)
-	PanicIfErr(err)
-	se, err := mgo.DialWithInfo(di)
+	if strings.Contains(mongodbUrl, "@") {
+		di, err := mgo.ParseURL(mongodbUrl)
+		PanicIfErr(err)
+		se, err := mgo.DialWithInfo(di)
+		PanicIfErr(err)
+		return se.DB("")
+	}
+	se, err := mgo.Dial(mongodbUrl)
 	PanicIfErr(err)
 	return se.DB("")
 }
